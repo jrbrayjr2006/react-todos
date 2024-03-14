fd10418a9735a0996b4dc4b2d89f2769597c6aef
James Bray <jrbrayjr@gmail.com>
2024-03-13 23:30:33 -0500

    add Todos component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todos from './components/Todos';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Todos />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/Todos.tsx
@@ -0,0 +1,7 @@
+export default function Todos() {
+    return (
+        <section>
+            <h1>Todos</h1>
+        </section>
+    );
+}
