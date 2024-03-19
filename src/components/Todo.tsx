a7dd57029fdd7ff9660479720749b638f50b9a6e
James Bray <jrbrayjr@gmail.com>
2024-03-18 22:02:45 -0500

    Create Todo component and utilize useState hook to add todo to list

---
+++ src/components/Todo.tsx
@@ -0,0 +1,8 @@
+export default function Todo({todoText}) {
+    return (
+        <div>
+            <input type="checkbox"></input> 
+            <span>{todoText}</span>
+        </div>
+    );
+}
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import Todo from './Todo';
+
+const TODO_ARRAY = ["My First Todo", "My Second Todo"];
+
 export default function Todos() {
+    const [todoList, setTodoList] = useState(TODO_ARRAY);
+    function addItemToTodoList(item: string) {
+        console.log("add new Todo to list of Todos");
+        setTodoList([...todoList, item]);
+    }
+
     return (
         <section>
-            <h1>Todos</h1>
+            <h2>Todos</h2>
+            <button onClick={() => addItemToTodoList("New item")}>Add Todo</button>
+            <ul>
+                {todoList.map(todo => 
+                    <li><Todo todoText={todo}/></li>
+                )}
+            </ul>
         </section>
     );
 }
