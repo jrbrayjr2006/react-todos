293be7d9109409db2ab4a561f68b8b41a3530bd5
James Bray <jrbrayjr@gmail.com>
2024-04-03 21:29:43 -0500

    clean up code

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Todo from './Todo';
 import MakeTodo from './MakeTodo';
-// import './Todos.css';
 
 const TODO_ARRAY: { id: number, title: string }[] = [];
 
